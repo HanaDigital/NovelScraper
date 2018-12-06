f1223c46e01e82d96d76d46019c50b3fd1999cd1
dr_nyt <unconfigured@null.spigotmc.org>
2018-12-06 23:11:37 +0400

    Universal Novel Scrape

--- HanaNovelScraper.py
+++ HanaNovelScraper.py
@@ -1,8 +1,11 @@
 from WuxiaScraper import WuxiaScraper
 from tkinter import *
 
-def msg(text, color='black'):
+def msg(text):
+    output.config(state='normal')
     output.insert(END, text + '\n')
+    output.see(END)
+    output.config(state='disabled')
 
 def compiler():
     link = eNovel.get()
@@ -16,11 +19,20 @@ def compiler():
 
     try:
         Novel = WuxiaScraper(link, volume)
+        Novel.start()
+        #bCompile.config(state='disabled')
+        msg('+'*20)
+        #msg('This can take ALOT of time so be patient')
+        #msg('DONOT close the app until you see files created in your directory')
+        #msg('+'*20)
     except:
-        msg('Error Occured!', 'red')
-        msg('Make sure provided link or volume num is valid.', 'red')
-    
-    
+        msg('+'*20)
+        msg('Error Occured!')
+        msg('+'*20)
+        msg('Make sure provided link or volume num is valid.')
+        msg('+'*20)
+        msg('')
+
 ## Window Creator
 window = Tk()
 window.title("Hana Novel Scraper")
@@ -42,6 +54,13 @@ eVolume.grid(row=1, column=1, sticky=W)
 Button(window, text="Compile", width=8, command=compiler).grid(row=2, column=1, sticky=W)
 
 #Text Boxes
-output = Text(window, width=75, height=10, wrap=WORD, background="white")
+output = Text(window, width=75, height=10, state='disabled', wrap=WORD, background="white")
 output.grid(row=3, column=0, padx=5, columnspan=2, sticky=W)
-output.insert(END,'LOG:\n')
+msg('LOG:')
+
+#Scroll Bars
+scroll = Scrollbar(window, width=10, command=output.yview)
+output.config(yscrollcommand=scroll.set)
+scroll.grid(row=3, column=1, sticky=E)
+
+window.mainloop()
--- WuxiaScraper.py
+++ WuxiaScraper.py
@@ -1,12 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+from HanaDocument import HanaDocument
 
 class WuxiaScraper(object):
 
     def __init__(self, link, volume=0):
+        self.HD = HanaDocument()
+        self.head = 0
+        self.novelName = ''
+        self.chapterNum_start = 1
+        self.chapterNum_end = 0
+        self.volumeNum = 0
+
         self.link = link
         self.volume = volume
-        if(self.volume - 1 != 0):
+        if(self.volume != 0):
             self.volume_limit = 1
         else:
             self.volume_limit = 0
@@ -14,37 +22,74 @@ class WuxiaScraper(object):
 
         page = requests.get(link)
         self.soup = BeautifulSoup(page.text, 'html.parser')
-            
-    def getVolume(self):
 
-        volume_list = soup.find_all(class_="panel-body")
+    def start(self):
+        self.getChapterLinks()
+        
+    def getMetaData(self, link_start, link_end):
+        metaData = []
+        
+        partsX = link_start.split('/')
+        for x in partsX:
+            if x != '' and x != 'novel':
+                metaData.append(x)
+        self.novelName = metaData[0].split('-')
+        self.novelName = self.novelName[0].capitalize() + ' ' + self.novelName[1].capitalize() + ' ' + self.novelName[2].capitalize()
+        self.chapterNum_start = int(metaData[1].split('-')[2])
+        
+        metaData = []
+
+        partsY = link_end.split('/')
+        for y in partsY:
+            if y != '' and y != 'novel':
+                metaData.append(y)
+        self.chapterNum_end = int(metaData[1].split('-')[2])
+                
+    def getChapterLinks(self):
+        vol = self.volume
+        volume_list = self.soup.find_all(class_="panel-body")
 
         for v in volume_list:
-
             chapter_links = []
+
+            self.HD.stylesConfig('Heading 1', 36)
+            self.HD.stylesConfig('Normal', 32)
             
             if v.find(class_="col-sm-6") == None:
                 continue
-
-            if self.volume - 1 != 0:
-                self.volume-=1
+            
+            #TODO add book cover feature
+            self.HD.sectionConfig(0.5)
+            
+            if vol - 1 != 0 and self.volume_limit == 1:
+                vol-=1
                 continue
             
             chapter_html_links = v.find_all(class_="chapter-item")
             for chapter_http in chapter_html_links:
                 chapter_links.append(chapter_http.find('a').get('href'))
-
+            
             self.volume_links.append(chapter_links)
 
-            if(volume_limit != 0):
+            self.getChapter()
+
+            self.getMetaData(chapter_links[0], chapter_links[-1])
+
+            self.volume_links = []
+            if(self.volume_limit == 1):
+                self.HD.saveBook(self.novelName, self.volume, self.chapterNum_start, self.chapterNum_end)
                 break
             
-        getChapterLink()
+            self.volume+=1
+            self.HD.saveBook(self.novelName, self.volume, self.chapterNum_start, self.chapterNum_end)
+            self.HD = HanaDocument()
+            print('OK')
             
     
-    def getChapterLink(self):
+    def getChapter(self):
         for v in self.volume_links:
             for chapters in v:
+                chapter_list = []
                 page = requests.get('https://www.wuxiaworld.com' + chapters)
                 soup = BeautifulSoup(page.text, 'html.parser')
                 story_view = soup.find_all(class_='p-15')
@@ -54,9 +99,14 @@ class WuxiaScraper(object):
                         chapter_list.append(story.get_text().replace('\xa0', ' ').replace('Previous Chapter', ''))
                 for paragraph in chapter_list:
                     if paragraph != '':
-                        
-                  
-                
-        
-    
-    
+                        if self.head == 0:
+                            self.HD.addHead(paragraph)
+                            self.head = 1
+                        else:
+                            self.HD.addPara(paragraph)
+                self.HD.addSection()
+                self.head = 0
+                break
+                print('OK')
+
+#Novel = WuxiaScraper('https://www.wuxiaworld.com/novel/martial-god-asura')
