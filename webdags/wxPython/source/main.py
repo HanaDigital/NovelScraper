71bc1d926644d6b9a4fc42dd726e9dbaf8116c68
webdagger <ayomipoe@gmail.com>
2019-06-12 16:50:33 +0100

    Added Update Checker Fixed msg() Removed class Log Made platform specific checks

--- webdags/wxPython/source/main.py
+++ webdags/wxPython/source/main.py
@@ -1,6 +1,10 @@
 import os
+import platform
 import wx
 import threading
+import requests
+import webbrowser
+from bs4 import BeautifulSoup
 from wuxiaworld_co import CoWuxiaPanel
 from wuxiaworld_com import WuxiaWorldPanel
 from novelplanet import NovelPlanetPanel
@@ -9,8 +13,11 @@ from novelplanet import NovelPlanetPanel
 # file dialog, used in the selection of book covers
 wildcard = "Image File (*.png)"
 # Version number
-version = "0.8"
-
+version = "0.7.4"
+# Platforms
+WINDOWS = (platform.system() == "Windows")
+LINUX = (platform.system() == "Linux")
+MAC = (platform.system() == "Darwin")
 
 # The launch panel
 class LaunchPanel(wx.Panel):
@@ -18,9 +25,11 @@ class LaunchPanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, id=wx.ID_ANY)
         # Check for new version
-        #self.version_control()
         self.SetBackgroundColour("BLACK")
-        self.SetForegroundColour("RED")
+        if MAC:
+            self.SetForegroundColour("RED")
+        else:
+            self.SetForegroundColour("WHITE")
         # widget to hold the text "Enter URL:
         self.enter_url_text = wx.StaticText(self, id=wx.ID_ANY, label="Enter Url: ")
         self.enter_url_text.SetFont(
@@ -46,8 +55,8 @@ class LaunchPanel(wx.Panel):
         # Box to display progress report and status
         self.log_report = wx.TextCtrl(self, id=wx.ID_ANY, size=(600, 300),
                                       style=wx.TE_MULTILINE | wx.TE_READONLY | wx.VSCROLL | wx.TE_RICH)
-        self.log = Log(self.log_report)
-        self.log.write("Log:")
+
+        self.msg("Log:")
         # Button to run the progran
         self.run_button = wx.Button(self, label="Compile")
         self.run_button.SetFont(
@@ -163,7 +172,7 @@ class LaunchPanel(wx.Panel):
                 self.select_cover_dialog_button.Enable()
         if which_site == "Wuxiaworld.com":
             if 'wuxiaworld.com' in url:
-                BookThread(self.WuxiaWorldPanel.run, which_site=which_site, **kwargs)
+                BookThread(self.wuxiaworld_panel.run, which_site=which_site, **kwargs)
             else:
                 self.log.write("\n\n\n Your link is invalid.")
                 self.log.write("\nSelect the correct source and enter a valid link.")
@@ -172,25 +181,20 @@ class LaunchPanel(wx.Panel):
                 self.select_cover_dialog_button.Enable()
         if which_site == "m.Wuxiaworld.co":
             if 'm.wuxiaworld.co' in url:
-                BookThread(self.CoWuxiaPanel.run, which_site=which_site, **kwargs)
+                BookThread(self.co_wuxiaworld_panel.run, which_site=which_site, **kwargs)
             else:
                 self.log.write("\n\n\n Your link is invalid.")
                 self.log.write("\nSelect the correct source and enter a valid link.")
                 self.log.write("\nFor m.wuxiaworld.co: https://m.wuxiaworld.co/Reverend-Insanity/")
                 self.run_button.Enable()
                 self.select_cover_dialog_button.Enable()
-
-
-class Log(object):
-
-    def __init__(self, wxTextCtrl):
-        self.out = wxTextCtrl
-
-    def write(self, string):
+    
+    def msg(self, string):
         # Enables and disables the log after writing
-        self.out.Enable()
-        wx.CallAfter(self.out.WriteText, string)
-        self.out.Disable()
+        self.log_report.Enable()
+        wx.CallAfter(self.log_report.WriteText, string)
+        self.log_report.ShowPosition(self.log_report.GetLastPosition())  # Set the position to the bottom
+        self.log_report.Disable()  # Disable the log after writing
 
 
 class BookThread(threading.Thread):
@@ -218,6 +222,17 @@ class BookThread(threading.Thread):
         elif self.which_site == "NovelPlanet":
             self.book_function(self.url, self.cover, self.min_chapter, self.max_chapter)
 
+# I put it in a different thread to prevent it from freezing the UI while it's calling requests
+# If there is no network, the frame does not show
+class UpdateThread(threading.Thread):
+    def __init__(self, parent):
+        threading.Thread.__init__(self)
+        self.parent = parent
+        self.daemon = True
+        self.start()
+    def run(self):
+        MainFrame.version_control(self.parent)
+
 
 class MainFrame(wx.Frame):
 
@@ -226,8 +241,30 @@ class MainFrame(wx.Frame):
         wx.Frame.__init__(self, parent=parent, style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX),
                           title="Nyt's Novel Downloader")
         panel = LaunchPanel(self)
+        #message = wx.MessageBox("Quit program?", "Confirm",
+        #                        wx.YES | wx.CANCEL,)
+        UpdateThread(self)
         self.Center()
         self.Show()
+
+    def version_control(self):
+        url = "https://pastebin.com/7HUqzRGT"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'lxml')
+        checkVersion = soup.find(class_='de1')
+        # TODO dr-nyt please verify if this implementation is correct
+        # TODO isn't it beter to use version < checkVersion?
+        if version not in checkVersion:
+            text = "New Update Available"
+            dlg = wx.MessageDialog(None,"Update found, \n A new version has been found", "Update Found", wx.OK)
+            dlg.ShowModal()
+            webbrowser.open_new(r"https://github.com/dr-nyt/Translated-Novel-Downloader/releases")
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app = wx.App()
--- webdags/wxPython/source/wuxiaworld_co.py
+++ webdags/wxPython/source/wuxiaworld_co.py
@@ -5,5 +5,15 @@ from bs4 import BeautifulSoup
 
 class CoWuxiaPanel(wx.Panel):
     def __init__(self, parent):
+        self.current_directory = parent.current_directory
         wx.Panel.__init__(self, parent, id=wx.ID_ANY)
         self.Hide()
+
+    def run(self, url, cover):
+        link = url
+        cover = cover
+        if cover == "":
+            cover = f'{self.current_directory}/cover.png'
+
+
+
