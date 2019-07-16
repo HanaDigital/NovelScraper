c17ac754b61a3336a9c9dab99dcb93d6548b8daa
dr_nyt <autumnwind101011@gmail.com>
2019-07-16 23:18:56 +0400

    version control 0.8.1

--- Version 0.8.1/main.py
+++ Version 0.8.1/main.py
@@ -10,7 +10,7 @@ from wuxiaworld_com import WuxiaWorldPanel
 from novelplanet import NovelPlanetPanel
 
 # Version number
-version = "0.8"
+version = "0.8.1"
 
 # file dialog, used in the selection of book covers
 wildcard = "Image file (*.png)" \
