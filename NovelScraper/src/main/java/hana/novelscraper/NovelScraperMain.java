19882235013cf6f2edc38c6a1c87b9e405cca24d
dr_nyt <autumnwind101011@gmail.com>
2019-07-16 22:23:43 +0400

    JAVA Rework

---
+++ NovelScraper/src/main/java/hana/novelscraper/NovelScraperMain.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hana.novelscraper;
+
+import hana.display.MainFrame;
+
+/**
+ *
+ * @author super
+ */
+public class NovelScraperMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+        mainFrame.addContent();
+        
+    }
+    
+}
