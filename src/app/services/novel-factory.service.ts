0701a2595a3fd089f26504830cc515089dc6f11a
Shehryar <shehryar.dev@gmail.com>
2021-02-27 23:47:53 +0400

    fixed novel cover and ads script - v2.0.4

--- src/app/services/novel-factory.service.ts
+++ src/app/services/novel-factory.service.ts
@@ -1,30 +1,36 @@
-import { Injectable } from '@angular/core';
-import { DatabaseService } from './database.service';
-import { novelObj, chapterObj } from '../resources/types';
-import shellJS from 'shelljs';	// For folder creation, idk why I didn't just do it with fs
-import { remote } from 'electron';
-import * as path from 'path';
-import rimraf from 'rimraf';	// For recursive folder deletion
-import epubGen from 'nodepub';
-import mime from 'mime';
-import { writeFileSync, createWriteStream, writeFile } from 'fs';
-import request from 'request';
+import { Injectable } from "@angular/core";
+import { DatabaseService } from "./database.service";
+import { novelObj, chapterObj } from "../resources/types";
+import shellJS from "shelljs"; // For folder creation, idk why I didn't just do it with fs
+import { remote } from "electron";
+import * as path from "path";
+import rimraf from "rimraf"; // For recursive folder deletion
+const epubGen = require("nodepub");
+import mime from "mime";
+import { writeFileSync, createWriteStream, writeFile } from "fs";
+import request from "request";
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: "root",
 })
 export class NovelFactoryService {
-
 	// Path to the downloads folder
-	downloadFolder = path.join(remote.app.getPath("downloads"), "NovelScraper-Library");
+	downloadFolder = path.join(
+		remote.app.getPath("downloads"),
+		"NovelScraper-Library"
+	);
 
-	constructor(public database: DatabaseService,) { }
+	constructor(public database: DatabaseService) {}
 
-	async generateEpub(novel: novelObj, chapters: chapterObj[], downloadID: number): Promise<void> {
+	async generateEpub(
+		novel: novelObj,
+		chapters: chapterObj[],
+		downloadID: number
+	): Promise<void> {
 		// Create the folder for storing the novel files
-		const novelFile = novel.name.replace(/[/\\?%*:|"<>]/g, '');
+		const novelFile = novel.name.replace(/[/\\?%*:|"<>]/g, "");
 		const novelFolder = novel.folderPath;
-		await shellJS.mkdir('-p', novelFolder);
+		await shellJS.mkdir("-p", novelFolder);
 
 		// Save chapters in a json file and set the novel as download
 		await this.saveChapters(novel, chapters, novelFolder);
@@ -34,34 +40,41 @@ export class NovelFactoryService {
 
 		// Set some meta data for the epub file
 		const metadata = {
-			id: '0000-0000-0001',
+			id: "0000-0000-0001",
+			cover: coverPath,
 			title: novel.name,
 			author: novel.author,
 			genre: novel.genre,
-			language: 'en',
-			description: 'This novel was downloaded from a pirate site using NovelScraper.'
-				+ ' If possible, please support the author: ' + novel.author,
-			contents: 'Table of Contents',
-			images: []
-		}
+			language: "en",
+			description:
+				"This novel was downloaded from a pirate site using NovelScraper." +
+				" If possible, please support the author: " +
+				novel.author,
+			contents: "Table of Contents",
+			images: [],
+		};
 
 		// Generate the epub file and add the chapters
-		const epub = epubGen.document(metadata, coverPath);
-		for (const chapter of chapters) epub.addSection(chapter.title, chapter.data);
-		epub.writeEPUB(
-			(error) => console.log(error),
-			novelFolder, novelFile,
-			() => console.log("[NOVEL FACTORY]: Novel saved!")
-		);
+		const epub = epubGen.document(metadata);
+		for (const chapter of chapters)
+			epub.addSection(chapter.title, chapter.data);
 
-		this.database.updateDownloading(novel.link, false);
-		this.database.updateDownloaded(novel.link, true);
-		this.database.updateIsUpdated(novel.link, true);
-		this.database.setDownloaded(downloadID);
+		setTimeout(async () => {
+			await epub.writeEPUB(novelFolder, novelFile);
+
+			this.database.updateDownloading(novel.link, false);
+			this.database.updateDownloaded(novel.link, true);
+			this.database.updateIsUpdated(novel.link, true);
+			this.database.setDownloaded(downloadID);
+		}, 2000);
 	}
 
 	// Save chapters to a json file so we don't re-download when updating
-	async saveChapters(novel: novelObj, chapters: chapterObj[], novelFolder: string): Promise<void> {
+	async saveChapters(
+		novel: novelObj,
+		chapters: chapterObj[],
+		novelFolder: string
+	): Promise<void> {
 		const chaptersFile = path.join(novelFolder, "chapters.json");
 		const json = JSON.stringify(chapters, null, 4);
 		writeFile(chaptersFile, json, (err) => {
@@ -69,18 +82,24 @@ export class NovelFactoryService {
 			else {
 				// Update total chapters of the novel
 				novel.downloadedChapters = chapters.length;
-				this.database.updateDownloadedChapters(novel.link, chapters.length);
+				this.database.updateDownloadedChapters(
+					novel.link,
+					chapters.length
+				);
 			}
-		})
+		});
 	}
 
 	// Download novel cover and return the path
-	async downloadCover(novelCover: string, novelFolder: string): Promise<string> {
+	async downloadCover(
+		novelCover: string,
+		novelFolder: string
+	): Promise<string> {
 		const coverExtension = mime.getExtension(mime.getType(novelCover));
 		const coverPath = path.join(novelFolder, "cover." + coverExtension);
 		const out = createWriteStream(coverPath);
 
-		const req = await request({ method: 'GET', uri: novelCover });
+		const req = await request({ method: "GET", uri: novelCover });
 		await req.pipe(out);
 
 		return coverPath;
--- src/app/services/sources/sourceService.ts
+++ src/app/services/sources/sourceService.ts
@@ -1,11 +1,10 @@
 import { chapterObj, novelObj, update } from "app/resources/types";
 import { existsSync, readFileSync } from "fs";
 import { DatabaseService } from "../database.service";
-const cloudscraper = (<any>window).require('cloudscraper');
-import * as path from 'path';
+const cloudscraper = (<any>window).require("cloudscraper");
+import * as path from "path";
 
 export class sourceService {
-
 	sourceNovels: novelObj[] = [];
 
 	searching = false;
@@ -14,13 +13,19 @@ export class sourceService {
 
 	scrollPos: number;
 
-	constructor(public database: DatabaseService,) { }
+	constructor(public database: DatabaseService) {}
 
-	async searchWIthLink(link: string, source: string, updatingInfo: boolean): Promise<novelObj> { return {}; }
+	async searchWIthLink(
+		link: string,
+		source: string,
+		updatingInfo: boolean
+	): Promise<novelObj> {
+		return {};
+	}
 
-	async searchWithName(name: string, source: string): Promise<void> { }
+	async searchWithName(name: string, source: string): Promise<void> {}
 
-	async download(novel: novelObj, downloadID: number): Promise<void> { }
+	async download(novel: novelObj, downloadID: number): Promise<void> {}
 
 	update(novel: novelObj, numOfChapters: number): update {
 		if (numOfChapters > novel.totalChapters) {
@@ -28,16 +33,16 @@ export class sourceService {
 			this.database.updateTotalChapters(novel.link, numOfChapters);
 		}
 
-		const updateFile = path.join(novel.folderPath, 'chapters.json');
+		const updateFile = path.join(novel.folderPath, "chapters.json");
 
 		if (!existsSync(updateFile)) {
 			novel.downloaded = false;
 			this.database.updateDownloaded(novel.link, false);
-			console.log("[SERVICE]: Missing update file. Downloading again!")
+			console.log("[SERVICE]: Missing update file. Downloading again!");
 			return { updateChapters: undefined, startIndex: 0 };
 		}
 
-		const json: string = readFileSync(updateFile, 'utf8');
+		const json: string = readFileSync(updateFile, "utf8");
 		const updateChapters: chapterObj[] = JSON.parse(json);
 
 		if (numOfChapters === novel.downloadedChapters) {
@@ -45,13 +50,18 @@ export class sourceService {
 			return { updateChapters: undefined, startIndex: -1 };
 		}
 
-		return { updateChapters: updateChapters, startIndex: updateChapters.length }
+		return {
+			updateChapters: updateChapters,
+			startIndex: updateChapters.length,
+		};
 	}
 
 	pushOrUpdateNovel(novel: novelObj, updatingInfo: boolean): void {
 		if (!novel.downloadedChapters) novel.downloadedChapters = 0;
 		if (!updatingInfo) {
-			this.sourceNovels = this.sourceNovels.filter(sourceNovel => sourceNovel.link !== novel.link);
+			this.sourceNovels = this.sourceNovels.filter(
+				(sourceNovel) => sourceNovel.link !== novel.link
+			);
 			this.sourceNovels.unshift(novel);
 		} else if (novel.totalChapters > novel.downloadedChapters) {
 			this.database.updateNovelObj(novel);
@@ -59,35 +69,57 @@ export class sourceService {
 		}
 	}
 
-	prepChapter(novel: novelObj, downloadID: number, title: string, chapterBody: string, currentPos: number, destPos: number): chapterObj {
+	prepChapter(
+		novel: novelObj,
+		downloadID: number,
+		title: string,
+		chapterBody: string,
+		currentPos: number,
+		destPos: number
+	): chapterObj {
 		chapterBody = chapterBody.replace(/<script.*<\/script>/gi, ""); // Remove any script tags usually used with ads (Security+)
 		chapterBody = chapterBody.replace(/<iframe.*<\/iframe>/gi, ""); // Remove any iframes usually used with ads
 		chapterBody = chapterBody.replace(/< *br *>/gi, "<br/>"); // Make sure all <br/> tags end correctly for xhtml
-		chapterBody = chapterBody.replace(/<br *\/ *br *>/gi, "");  // Remove any useless </br> tags
-		chapterBody = chapterBody.replace(/(<img("[^"]*"|[^\/">])*)>/gi, "$1/>"); // Make sure img tag ends correctly for xhtml
+		chapterBody = chapterBody.replace(/<br *\/ *br *>/gi, ""); // Remove any useless </br> tags
+		chapterBody = chapterBody.replace(
+			/(<img("[^"]*"|[^\/">])*)>/gi,
+			"$1/>"
+		); // Make sure img tag ends correctly for xhtml
 
 		chapterBody += this.addPropoganda(novel.author);
 
-		const percentage = (((currentPos / destPos) * 100).toFixed(2));
+		const percentage = ((currentPos / destPos) * 100).toFixed(2);
 		this.database.updateDownloadTracker(downloadID, percentage);
 		console.log(percentage);
 
-		return { title: title, data: chapterBody, read: false, scroll: 0 } as chapterObj;
+		return {
+			title: title,
+			data: chapterBody,
+			read: false,
+			scroll: 0,
+		} as chapterObj;
 	}
 
 	async getHtml(link: string): Promise<HTMLHtmlElement> {
-		const stringHtml: string = await cloudscraper({ method: 'GET', url: link, }, (error, response, novelHtmlString) => {
-			return novelHtmlString;
-		});
+		const stringHtml: string = await cloudscraper(
+			{ method: "GET", url: link },
+			(error, response, novelHtmlString) => {
+				return novelHtmlString;
+			}
+		);
 
-		const html = document.createElement('html');
+		const html = document.createElement("html");
 		html.innerHTML = stringHtml;
 		return html;
 	}
 
 	addPropoganda(author: string): string {
-		return "<br/><br/>" +
+		return (
+			"<br/><br/>" +
 			"<p>This novel was scraped from a pirate site using NovelScraper.</p>" +
-			"<p>If possible, please support the author(s) of this novel: " + author + "</p>";
+			"<p>If possible, please support the author(s) of this novel: " +
+			author +
+			"</p>"
+		);
 	}
 }
